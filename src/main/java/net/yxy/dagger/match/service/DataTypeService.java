1c22b7b6cc2dc08e182ebeb3eebc8ddeeed20e9b
Xianyi Ye <yexianyi@hotmail.com>
2017-06-16 15:45:29 +0800

    implement function param parsing and combines

--- src/main/java/net/yxy/dagger/match/service/DataTypeService.java
+++ src/main/java/net/yxy/dagger/match/service/DataTypeService.java
@@ -1,17 +1,132 @@
 package net.yxy.dagger.match.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.codehaus.jettison.json.JSONException;
 
 public class DataTypeService {
 	
 	private static Map<String, String> dataTypeMapping = new HashMap<String, String>() ;
 	private static Map<String, String> dataTypeTransformMapping = new HashMap<String, String>() ;
+	private static Map<String, Object> dataTypes = null ;
+	
+	static{
+		initDataTypes() ;
+	}
+	
+	private static void initDataTypes() {
+		dataTypes = new HashMap<String , Object>(){{  
+		     put("~any", new HashMap<String , Object>(){{
+		    	 put("~number", new HashMap<String , Object>(){{
+	    		 		put("~whole_number",new HashMap<String , Object>(){{
+	    		 				put("@bit", "@bit") ;
+	    		 				put("@integer", "@integer") ;
+	    		 				put("@smallint", "@smallint") ;
+	    		 				put("@tinyint", "@tinyint") ;
+	    		 				put("@bigint", "@bigint") ;
+	    		 		}}) ;
+	    		 		put("~floating_point",new HashMap<String , Object>(){{
+			 				put("@float", "@float") ;
+			 				put("@real", "@real") ;
+			 				put("@double", "@double") ;
+			 				put("@decimal", "@decimal") ;
+			 				put("@numeric", "@numeric") ;
+			 		}}) ;
+		    	 }});
+		    	 put("~string", new HashMap<String , Object>(){{
+		    		 	put("@char","@char");
+		    		 	put("@varchar","@varchar");
+		    		 	put("@longvarchar","@longvarchar");
+		    		 	put("@nchar","@nchar");
+		    		 	put("@varchar2","@varchar2");
+		    		 	put("@nvarchar","@nvarchar");
+		    	 }});
+		    	 put("~binary", new HashMap<String , Object>(){{
+		    		 	put("@binary","@binary");
+		    		 	put("@varbinary","@varbinary");
+		    	 }});
+		    	 put("~date", new HashMap<String , Object>(){{
+		    		 	put("@date","@date");
+		    		 	put("@time","@time");
+		    		 	put("@timestamp","@timestamp");
+		    	 }});
+		    	 put("~lob", new HashMap<String , Object>(){{
+		    		 	put("@clob","@clob");
+		    		 	put("@blob","@blob");
+		    	 }});
+		    	 put("@boolean", "@boolean");
+		    	 put("@null", "@null");
+		    	 put("~interval_year", new HashMap<String , Object>(){{
+		    		 	put("@interval_year_to_month","@interval_year_to_month");
+		    		 	put("@interval_year","@interval_year");
+		    		 	put("@interval_month","@interval_month");
+		    	 }});
+		    	 put("~interval_day", new HashMap<String , Object>(){{
+		    		 	put("@interval_day","@interval_day");
+		    		 	put("@interval_hour","@interval_hour");
+		    		 	put("@interval_minute","@interval_minute");
+		    		 	put("@interval_second","@interval_second");
+		    		 	put("@interval_day_to_hour","@interval_day_to_hour");
+		    		 	put("@interval_day_to_minute","@interval_day_to_minute");
+		    		 	put("@interval_day_to_second","@interval_day_to_second");
+		    		 	put("@interval_hour_to_minute","@interval_hour_to_minute");
+		    		 	put("@interval_hour_to_second","@interval_hour_to_second");
+		    		 	put("@interval_minute_to_second","@interval_minute_to_second");
+		    	 }});
+		    	 put("@xml", "@xml");
+		     }});  
+		 }};  
+	}
+	
+	public List<String> getDataTypesByTag(String tag){
+		List<String> res = new ArrayList<String>() ;
+		getDataTypesByTag(tag, dataTypes, res, false) ;
+		return res ;
+	}
+	
+	private void getDataTypesByTag(String tag, Map<String, Object> datatypeMap, List<String> res, boolean ifStored){
+		for (String key : datatypeMap.keySet()) {  
+			Object val = datatypeMap.get(key) ;
+		    if(key.equalsIgnoreCase(tag)){
+		    	if(val instanceof String || ifStored==true){
+		    		res.add((String) val) ;
+		    	}else if(val instanceof Map){
+		    		Map<String, Object> subMap = (Map<String, Object>) val ;
+		    		res.add((String) key) ;
+		    		getDataTypesByTag(tag, subMap, res, true) ;
+		    	}
+		    }else{
+		    	if(val instanceof Map){
+		    		Map<String, Object> subMap = (Map<String, Object>) val ;
+		    		if(ifStored){
+		    			res.add((String) key) ;
+		    			getDataTypesByTag(tag, subMap, res, true) ;
+		    		}else{
+		    			getDataTypesByTag(tag, subMap, res, false) ;
+		    		}
+		    	}else{
+		    		if(ifStored){
+		    			res.add((String) val) ;
+		    		}
+		    	}
+		    } 
+		  
+		}  
+		
+		
+	}
+	
+	
+	
 	
 	public void addDataTypeMapping(String underlying_dt, String server_dt){
 		dataTypeMapping.put(underlying_dt, server_dt) ;
 	}
 	
+
 	public Map<String, String> getDataTypeMapping(){
 		return dataTypeMapping ;
 	}
@@ -25,9 +140,13 @@ public class DataTypeService {
 	}
 	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws JSONException {
+		DataTypeService dtService = new DataTypeService() ;
+		List<String> res = dtService.getDataTypesByTag("~any");
+		for(String dataType : res){
+			System.out.println(dataType);
+		}
+		
 	}
 
 }
--- src/main/java/net/yxy/dagger/match/service/FunctionService.java
+++ src/main/java/net/yxy/dagger/match/service/FunctionService.java
@@ -4,11 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLXML;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,20 +32,28 @@ import java.util.regex.Pattern;
 public class FunctionService {
 
 	private static Map<String, String> standardFunctionMap = new LinkedHashMap<String, String>()  ;
+	private static Map<String, String> funcMappings = new LinkedHashMap<String, String>()  ;
+	
 	
 	static{
-		loadFile() ;
+		loadFile(standardFunctionMap, "/functionEntities") ;
+		loadFile(funcMappings, "/FunctionMappings") ;
 	}
 	
-	private static void loadFile(){
+	private static void loadFile(Map<String, String> storeMap, String file_path){
 		BufferedReader br = null ;
 		try {
-			br = new BufferedReader(new FileReader(FunctionService.class.getResource("/functionEntities").getPath()));
+			br = new BufferedReader(new FileReader(FunctionService.class.getResource(file_path).getPath()));
 			
 			String line;
 		    while ((line = br.readLine()) != null) {
 		    	if(line.trim().length()>0 && !line.trim().startsWith("#")){
-			    	standardFunctionMap.put(line, null) ;
+		    		String[] array = line.trim().split(":") ;
+		    		if(array.length>1){
+		    			storeMap.put(array[0], array[1]) ;
+		    		}else{
+		    			storeMap.put(line, null) ;
+		    		}
 			    }
 		    }
 
@@ -74,15 +98,172 @@ public class FunctionService {
 		return resultSet ;
 	}
 	
+	private Connection getConnection(String dbUrl, String dbName, String userName, String password) throws SQLException {
+
+	    Connection conn = null;
+	    Properties connectionProps = new Properties();
+	    connectionProps.put("user",userName);
+	    connectionProps.put("password", password);
+
+        conn = DriverManager.getConnection(dbUrl, connectionProps);
+	    System.out.println("Connected to database");
+	    return conn;
+	}
 	
+	public void CreateTestSchema(){
+		
+	}
 	
+	public Map<String, String> testFunction(String funcDefStr, String funcSqlStr){
+		//funcDefStr =  CORR(~number,~string) ;
+		Pattern pattern = Pattern.compile("\\(.*?\\)");
+		Matcher m = pattern.matcher(funcDefStr);
+		if(m.find()){
+			String paramStr = m.group() ;
+			paramStr = paramStr.substring(1, paramStr.length()) ;
+			Connection conn = null;
+			PreparedStatement ps ;
+			try {
+				conn = getConnection("jdbc:impala://localhost:21050/", "test", "", "");
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			String[] params = paramStr.split(",") ;
+			Map<Integer, List<String>> paramMap = new HashMap<Integer, List<String>>() ;
+			DataTypeService dtService = new DataTypeService() ;
+			for(int i=0; i<params.length; i++){
+				List<String> datatypes = dtService.getDataTypesByTag(params[i].trim());
+				paramMap.put(i, datatypes) ;
+			}
+			
+			List<String[]> paramCombinates = getFuncParamCombinations(paramMap) ;
+			for(int i=paramCombinates.size()-1; i>=0; i--){
+				String[] paramArray = paramCombinates.get(i) ;
+				boolean isSuccess = executeFunction(conn, funcSqlStr, paramArray) ;
+			}
+			
+		}		
+		
+		
+		 Map<String, String> resultMap = new LinkedHashMap<String, String>() ;
+		 return resultMap ;
+		
+	}
+	
+
+	private boolean executeFunction(Connection conn, String funcSqlStr, String[] paramArray) {
+		String sql = "select " + funcSqlStr + " from test.alldatatypes;" ;
+		PreparedStatement ps = null ;
+		try {
+			ps = conn.prepareStatement(sql);
+			for(int i=0; i<paramArray.length; i++){
+				String paramName = paramArray[i] ;
+				Object objVal = null ;
+				switch(paramName){
+					case "@bit": 		
+					case "@boolean":	ps.setBoolean(i+1,(boolean) objVal); break;
+					case "@integer": 	ps.setInt(i+1, (int) objVal); break;
+					case "@smallint":	ps.setShort(i+1, (short) objVal); break;
+					case "@tinyint":	ps.setByte(i+1, (byte) objVal); break;
+					case "@bigint":		ps.setLong(i+1, (long) objVal); break;
+					case "@float":		ps.setLong(i+1, (long) objVal); break;
+					case "@real":		ps.setFloat(i+1, (float) objVal); break;
+					case "@double":		ps.setDouble(i+1, (double) objVal); break;
+					case "@decimal":	
+					case "@numeric":	ps.setBigDecimal(i+1, (BigDecimal) objVal); break;
+					case "@char":		
+					case "@varchar":	
+					case "@varchar2":	ps.setString(i+1, (String) objVal); break;
+					case "@longvarchar":
+					case "@nchar":		
+					case "@nvarchar":	ps.setNString(i+1,(String) objVal); break;
+					case "@binary":		ps.setBinaryStream(i+1, null); break;
+					case "@varbinary":	ps.setBytes(i+1, (byte[]) objVal); break;
+					case "@date":		ps.setDate(i+1, (Date) objVal); break;
+					case "@time":		ps.setTime(i+1, (Time) objVal); break;
+					case "@timestamp":	ps.setTimestamp(i+1, (Timestamp) objVal); break;
+					case "@clob":		ps.setClob(i+1,(Clob) objVal); break;
+					case "@blob":		ps.setBlob(i+1,(Blob) objVal); break;
+					case "@null":		ps.setNull(i+1,Types.NULL); break;
+					case "@interval_year_to_month":	
+					case "@interval_year":	
+					case "@interval_day":	
+					case "@interval_hour":	
+					case "@interval_minute":	
+					case "@interval_second":	
+					case "@interval_day_to_hour":	
+					case "@interval_day_to_minute":	
+					case "@interval_day_to_second":	
+					case "@interval_hour_to_minute":
+					case "@interval_hour_to_second":	
+					case "@interval_minute_to_second":	break ;
+					case "@xml":		ps.setSQLXML(i+1,(SQLXML) objVal); break;
+						
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally{
+			if(ps!=null){
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+		}
+		
+		return false;
+	}
+
+
+	public List<String[]> getFuncParamCombinations(Map<Integer, List<String>> datatypes){
+		List<String[]> combinates = new ArrayList<String[]>() ;
+		getFuncParamCombinations(datatypes, 0, new String[datatypes.size()], combinates) ;
+		return combinates ;
+	}
+	
+	private void getFuncParamCombinations(Map<Integer, List<String>> datatypes, int currIdx, String[] resArray, List<String[]> resList){
+		if(currIdx==datatypes.size()){
+//			if(resArray[0].contains("~") || resArray[1].contains("~") || resArray[2].contains("~")){
+//				return ;	
+//			}
+//			System.out.println(resArray[0]+", "+resArray[1]+", "+resArray[2]);
+			String[] combinates = new String[resArray.length] ;
+			System.arraycopy(resArray, 0, combinates, 0, resArray.length) ;
+			resList.add(combinates) ;
+			return ;
+		}
+		List<String> datatypeList = datatypes.get(currIdx) ;
+		for(int i=0; i<datatypeList.size(); i++){
+			resArray[currIdx] = datatypeList.get(i) ;
+			getFuncParamCombinations(datatypes, currIdx+1, resArray, resList) ;
+		}
+		
+	}
 	
 	
 	public static void main(String[] args){
-		String str = "min_int" ;
-		   Pattern r = Pattern.compile("\\bmin_int\\b", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-		     Matcher m = r.matcher(str);
-		System.out.println(str.matches("\\b(?i)min_int\\b"));
+		FunctionService funcService = new FunctionService() ;
+		DataTypeService dtService = new DataTypeService() ;
+		List<String> datatypesParam1 = dtService.getDataTypesByTag("~number") ;
+		List<String> datatypesParam2 = dtService.getDataTypesByTag("~string") ;
+		List<String> datatypesParam3 = dtService.getDataTypesByTag("~date") ;
+		
+		Map<Integer, List<String>> paramMap = new HashMap<Integer, List<String>>() ;
+		paramMap.put(0, datatypesParam1) ;
+		paramMap.put(1, datatypesParam2) ;
+		paramMap.put(2, datatypesParam3) ;
+		
+		List<String[]> resList = funcService.getFuncParamCombinations(paramMap) ;
+		
+		for(String[] array : resList){
+			System.out.println(array[0]+", "+array[1]+", "+array[2]);
+		}
+		
 		
 	}
 
